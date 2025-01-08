854551027164286ec107bf31fb03ad5e97860a16
unknown <snehanayak633@gmail.com>
2025-01-08 16:43:01 +0530

    Initial Commit

---
+++ Basics.py
@@ -0,0 +1,14 @@
+# Scripting way of writing the code:
+print('Scripting way of writing the code: Hello World')
+
+# Procedural way of writing the code:
+def message():
+    print('Procedural way of writing the code: Hello World')
+message()
+
+# Object Oriented way of writing the code:
+class Demo:
+    def instance_method(self):
+        print('Object Oriented way of writing the code: Hello World')
+d1 = Demo()
+d1.instance_method()
