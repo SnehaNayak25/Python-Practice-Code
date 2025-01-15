2e9e69ff25a593dcc580d59ab8358e728e53dda2
unknown <snehanayak633@gmail.com>
2025-01-15 20:22:24 +0530

    Modified the file

---
+++ Basic-Concepts/DataTypes.py
@@ -0,0 +1,21 @@
+
+a = 'Kod'
+print(type(a))  # str
+
+a = 'K'
+print(type(a))  # str
+
+a = 34
+print(type(a))  # int
+
+a = True
+print(type(a))  # bool
+
+a = False
+print(type(a))  # bool
+
+a = 3+4j
+print(type(a))  # Complex number which has real and imaginary part
+
+a = 6j
+print(type(a))  # Complex number
---
+++ Basic-Concepts/Overview.py
@@ -0,0 +1,8 @@
+'''
+
+ 1. Python is dynamically typed programming language.
+ 2. In Python while declaring variables, we no need to mention datatypes
+ 3. Datatypes will be assigned by language based on value stroed into the variable.
+
+ 
+ '''
