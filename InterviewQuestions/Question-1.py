760f9461f445bae7bf0b04ead68275cddb61f8ba
unknown <snehanayak633@gmail.com>
2025-01-15 18:27:41 +0530

    Interview Questions

---
+++ InterviewQuestions/Question-1.py
@@ -0,0 +1,14 @@
+for y in range(1,11):
+    z = 200
+
+def display(a):
+    print(a)
+    y = 100
+    print(y)
+
+display(50)
+print(y) # 10 here for loop y value is displayed , for loop y will has global scope ( display function y will not come because it is local variable)
+print(z) # 200
+# print(a) we will get error because a is the local variable
+
+# 1. Explain Login reuired decorator
