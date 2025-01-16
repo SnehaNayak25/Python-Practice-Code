525c38e1866b2a3aaa9f1749bd439700aecaa880
unknown <snehanayak633@gmail.com>
2025-01-16 18:57:18 +0530

    Tuple file modified

--- Advanced-Data-Types/Tuple.py
+++ Advanced-Data-Types/Tuple.py
@@ -34,3 +34,14 @@ t1 = (10,20,30)
 t2 = (40,50)
 t3 = t1+t2
 print(f"After concatenation: {t3}")
+
+# Tuple with single element should be ended with , (comma)
+tup = (10,)
+print(tup,type(tup)) #(10,) <class 'tuple'>
+
+# Unpacking of tuple
+new_tup = (10,20,30,40)
+# ele1 = new_tup[0]
+# ele2 = new_tup[1]
+ele1,ele2,ele3,ele4 = new_tup
+print(f"Value of element1: {ele1}")
