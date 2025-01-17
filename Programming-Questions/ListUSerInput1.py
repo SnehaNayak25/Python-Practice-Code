d48c7ac599bf4e1948e103039dbb63961a75dadc
unknown <snehanayak633@gmail.com>
2025-01-17 17:30:33 +0530

    List User Input Method1

---
+++ Programming-Questions/ListUSerInput1.py
@@ -0,0 +1,19 @@
+li = []
+num = int(input('Enter count: '))
+for i in range(num):
+    ele = int(input(f'Enter element at index {i}: '))
+    li.append(ele)
+print(li)
+
+'''
+Output:
+
+Enter count: 5
+Enter element at index 0: 1
+Enter element at index 1: 2
+Enter element at index 2: 3
+Enter element at index 3: 4
+Enter element at index 4: 5
+[1, 2, 3, 4, 5]
+
+'''
